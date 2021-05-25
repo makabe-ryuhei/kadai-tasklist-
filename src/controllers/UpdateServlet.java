dd617f2451b778c94b35c423ef0c325025ff90a3
ryuhei.makabe <r-makabe@systemstation.jp>
2021-05-25 09:35:23 +0900

    Add DestroyServlet.java

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -38,7 +38,7 @@ public class UpdateServlet extends HttpServlet {
         if(_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
-            // セッションスコープからメッセージのIDを取得して
+            // セッションスコープからタスクのIDを取得して
             // 該当のIDのメッセージ1件のみをデータベースから取得
             task m = em.find(task.class, (Integer)(request.getSession().getAttribute("task_id")));
 
